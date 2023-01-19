896aa36e1b8c2e52fd3878fe46769b77cefe1031
ideaexpert <rrayyan213@gmail.com>
2023-01-19 13:38:07 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName('navbar-brand')[0]
+const navbarLinks = document.getElementsByClassName('nav-link')[0]
+
+navbar-brand.addEventListener('click', () => {
+  nav-link.classList.toggle('active')
+})
